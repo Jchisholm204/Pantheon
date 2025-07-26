2db296dc1579fc88e33bdb172768753d98c1dac6
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-07-25 22:50:29 -0700

    reorg

--- sim/sources.py
+++ sim/sources.py
@@ -1,52 +1,79 @@
+import os
+from pathlib import Path
 
-ISA_SOURCES = [
-    "processor/rv32_isa.svh"
+
+class SourceFiles(list):
+    pass
+
+
+class Sources:
+    def get_basepath():
+        basepath = Path(__file__).resolve()
+        # Remove the file name
+        basepath = basepath.parent
+        basestring = str(basepath)
+        while not basestring.endswith("sim") and basestring != "/home":
+            basepath = basepath.parent
+            basestring = str(basepath)
+        if basestring == "/home":
+            assert "Path Resolution Failure"
+        return basepath.parent
+
+    def get_path(folder_name: str) -> Path:
+        base = str(Sources.get_basepath())
+        base += f"/{folder_name}"
+        return Path(base)
+
+    def get_sourceFiles(path: [Path, str], pattern="*") -> list:
+        if path is str:
+            path = Path(path)
+        path = path.rglob(pattern)
+        ls = SourceFiles()
+        for x in path:
+            ls.append(str(x))
+        return ls
+
+    def TYPES_SOURCES() -> SourceFiles:
+        basepath = Sources.get_path("include/types")
+        return Sources.get_sourceFiles(basepath)
+
+    ISA_SOURCES = [
+        "processor/rv32_isa.svh"
+    ]
+
+    def ALU_SOURCES() -> SourceFiles:
+        basepath = Sources.get_path("processor/ALU")
+        return Sources.get_sourceFiles(basepath)
+
+    MEM_SOURCES = [
+        "processor/memory/DMEM.sv",
+        "processor/memory/IMEM.sv",
+        "processor/memory/RAMBlock.sv",
+        "processor/memory/ROMBlock.sv",
+        "processor/memory/Register.sv",
+        "processor/memory/RegisterFile.sv"
+    ]
+
+    WISHBONE_SOURCES = [
+            "processor/../interfaces/WISHBONE_IF.sv"
+    ]
+
+    PIPE_SOURCES = [
+        "processor/datapipe/IF.sv",
+        "processor/datapipe/ID.sv",
+        "processor/datapipe/EX.sv",
+        "processor/datapipe/ME.sv"
 ]
 
-TYPES_SOURCES = [
-    "processor/types/reg_transport.svh",
-    "processor/types/pipeline_types.svh",
-    "processor/types/debug_types.svh"
-]
+    CTRL_SOURCES = [
+        "processor/control/BranchOutcome.sv",
+        "processor/control/HazardUnit.sv",
+        "processor/control/PC.sv",
+        "processor/control/decoder.sv",
+    ]
 
-ALU_SOURCES = [
-    "processor/ALU/CLA.sv",
-    "processor/ALU/BitWise.sv",
-    "processor/ALU/DIV32.sv",
-    "processor/ALU/MUL32.sv",
-    "processor/ALU/SHIFT.sv",
-    "processor/ALU/ALU.sv"
-]
-
-MEM_SOURCES = [
-    "processor/memory/DMEM.sv",
-    "processor/memory/IMEM.sv",
-    "processor/memory/RAMBlock.sv",
-    "processor/memory/ROMBlock.sv",
-    "processor/memory/Register.sv",
-    "processor/memory/RegisterFile.sv"
-]
-
-WISHBONE_SOURCES = [
-        "processor/../interfaces/WISHBONE_IF.sv"
-]
-
-PIPE_SOURCES = [
-    "processor/datapipe/IF.sv",
-    "processor/datapipe/ID.sv",
-    "processor/datapipe/EX.sv",
-    "processor/datapipe/ME.sv"
-]
-
-CTRL_SOURCES = [
-    "processor/control/BranchOutcome.sv",
-    "processor/control/HazardUnit.sv",
-    "processor/control/PC.sv",
-    "processor/control/decoder.sv",
-]
-
-INTERFACE_SOURCES = [
-    "processor/../interfaces/WISHBONE_IF.sv",
-    "processor/../interfaces/DBG_IF.sv",
-    "processor/../interfaces/BBUS_IF.sv"
-]
+    INTERFACE_SOURCES = [
+        "processor/../interfaces/WISHBONE_IF.sv",
+        "processor/../interfaces/DBG_IF.sv",
+        "processor/../interfaces/BBUS_IF.sv"
+    ]
---
+++ sim/test_sources.py
@@ -0,0 +1,13 @@
+import sources
+import string
+
+def main():
+    s = sources.Sources
+    ts = []
+    print(s.TYPES_SOURCES())
+    # for x in s.TYPES_SOURCES():
+    #     ts.append(str(x))
+    # print(ts)
+
+if __name__ == "__main__":
+    main()
